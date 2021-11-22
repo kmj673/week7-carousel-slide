6a7b07f2e0b27f93f9504c10cf145d671cd2451c
Minju Kim <kmj673@gmail.com>
2021-11-22 22:52:33 +0000

    added prev button and next button funtion

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-// find the prev,next button, slides and slider with queryselector
+// grab the prev,next button, slides and slider with queryselector
 prevButton = document.querySelector(".prev-button");
 nextButton = document.querySelector(".next-button");
 slides = document.querySelector(".slide-container").children;
+
 index = document.querySelector(".slider");
 
 //button
-function updateDOM() {}
 // get button clicked
-prevButton.addEventListener("click", updateDOM);
-// click the button then funtion will happen
+prevButton.addEventListener("click", updatePrev);
+nextButton.addEventListener("click", updateNext);
 
-// next or previous pcitur will be shown as main
+// update the slide for each button
+function updatePrev() {
+  console.log("this is prev button");
+}
+
+function updateNext() {
+  console.log(`this is next button`);
+}
 
 //slider
 // get images number and slider's location value
