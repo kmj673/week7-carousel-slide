dafdf47df47275b6371eae795c7e787a9668a4f8
Minju Kim <kmj673@gmail.com>
2021-11-22 22:22:09 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// find the prev,next button, slides and slider with queryselector
+prevButton = document.querySelector(".prev-button");
+nextButton = document.querySelector(".next-button");
+slides = document.querySelector(".slide-container").children;
+index = document.querySelector(".slider");
+
+//button
+function updateDOM() {}
+// get button clicked
+prevButton.addEventListener("click", updateDOM);
+// click the button then funtion will happen
+
+// next or previous pcitur will be shown as main
+
+//slider
+// get images number and slider's location value
